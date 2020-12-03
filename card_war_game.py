3d81fb38e58d3ffa9d78e509ad9f8be2670e4118
MohdTabish008 <72790916+MohdTabish008@users.noreply.github.com>
2020-12-03 20:29:59 +0530

    Update card_war_game.py

--- card_war_game.py
+++ card_war_game.py
@@ -1,8 +1,32 @@
+# For this project you will be using OOP to create a card game. This card game will
+# be the card game "War" for two players, you an the computer. If you don't know
+# how to play "War" here are the basic rules:
+
+# The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
+# face down. Anyone may deal first. Each player places his stack of cards face down,
+# in front of him.
+
+# The Play:
+
+# Each player turns up a card at the same time and the player with the higher card
+# takes both cards and puts them, face down, on the bottom of his stack.
+
+# If the cards are the same rank, it is War. Each player turns up three cards face
+# down and one card face up. The player with the higher cards takes both piles
+# (six cards). If the turned-up cards are again the same rank, each player places
+# another card face down and turns another card face up. The player with the
+# higher card takes all 10 cards, and so on.
 from random import shuffle
 SUITE = 'H D S C'.split()
 RANKS = '2 3 4 5 6 7 8 9 10 J K Q A'.split()
 
 class Deck:
+    """
+    This is the Deck Class. This object will create a deck of cards to initiate
+    play. You can then use this Deck list of cards to split in half and give to
+    the players. It will use SUITE and RANKS to create the deck. It should also
+    have a method for splitting/cutting the deck in half and Shuffling the deck.
+    """
     def __init__(self):
         print('Creating new Deck')
         self.allcards = [(s,r) for s in SUITE for r in RANKS]
@@ -13,6 +37,10 @@ class Deck:
         return (self.allcards[:26],self.allcards[26:])
 
 class Hand:
+    '''
+    This is the Hand class. Each player has a Hand, and can add or remove
+    cards from that hand. There should be an add and remove card method here.
+    '''
     def __init__(self,cards):
         self.cards = cards
     def __str__(self):
@@ -23,6 +51,10 @@ class Hand:
         return self.cards.pop()
 
 class Player:
+    """
+    This is the Player class, which takes in a name and an instance of a Hand
+    class object. The Payer can then play cards and check if they still have cards.
+    """
     def __init__(self,name,hand):
         self.name = name
         self.hand = hand
